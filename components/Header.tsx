3ea04ecf5e586dd5989804fb51b49aac033388fc
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-09 16:25:24 -0300

    changing spacing in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   const navigation = useNavigation();
 
   return (
-      <View className="w-full flex flex-row items-center justify-between bg-blue-500 pt-6 p-2">
+      <View className="w-full flex flex-row items-center justify-between bg-blue-500 pt-6 pb-2 px-4">
         <Text className="text-3xl text-white font-semibold">Lumina</Text>
         <TouchableOpacity onPress={(navigation as any).navigate("Home")}>
             <Image source={require("../assets/images/userIcon.png")} className="h-16 w-16 rounded-full" />
