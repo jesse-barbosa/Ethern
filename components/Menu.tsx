e39d2213bba5849db5b93d9640539f9ccd2825db
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-09 17:22:05 -0300

    feat: selected page indicator in Menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,17 +1,18 @@
 import { View, TouchableOpacity, Text } from "react-native";
 import { ClipboardList, Settings } from "lucide-react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 export default function Menu() {
   const navigation = useNavigation();
+  const route = useRoute();
 
   return (
       <View className="w-full flex flex-row items-center justify-around bg-blue-500 py-2">
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3 bg-blue-600">
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className={`${route.name === "Home" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-3`}>
             <ClipboardList size={28} color='#fff' />
             <Text className="text-md mt-1 text-white">Tarefas</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className="flex items-center rounded-lg p-3">
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className={`${route.name === "Settings" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-3`}>
             <Settings size={28} color='#fff' />
             <Text className="text-md mt-1 text-white">Configs</Text>
         </TouchableOpacity>
