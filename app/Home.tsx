97c61930e0233b602a40e8c46d8613a57e598ba7
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-09 15:50:11 -0300

    feat: creating menu component and importing it in home

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,17 +1,21 @@
 import { ScrollView, View, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
+import Menu from "../components/Menu";
 
 export default function Home() {
   const user = useSelector((state: RootState) => state.user);
 
   return (
-    <ScrollView>
-      <View>
-        <Text className="font-extrabold">Hello World!</Text>
-        <Text>User: { user?.name } </Text>
-        <Text>Email: { user?.email } </Text>
-      </View>
-    </ScrollView>
+    <View className="flex-1">
+      <ScrollView>
+        <View>
+          <Text className="font-extrabold">Hello World!</Text>
+          <Text>User: { user?.name } </Text>
+          <Text>Email: { user?.email } </Text>
+        </View>
+      </ScrollView>
+      <Menu />
+    </View>
   );
 }
---
+++ components/Menu.tsx
@@ -0,0 +1,20 @@
+import { View, TouchableOpacity, Text } from 'react-native';
+import { ClipboardList, Settings } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Menu() {
+  const navigation = useNavigation();
+
+  return (
+      <View className="w-full flex flex-row items-center justify-around bg-blue-500 py-2">
+        <TouchableOpacity onPress={(navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3 bg-blue-600">
+            <ClipboardList size={28} color='#fff' />
+            <Text className="text-md mt-1 text-white">Tarefas</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={(navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3">
+            <Settings size={28} color='#fff' />
+            <Text className="text-md mt-1 text-white">Configs</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
