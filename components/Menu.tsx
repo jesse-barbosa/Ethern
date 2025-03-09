684daaf4b3af8506af06623037e7c75f55b3b5d5
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-09 15:55:17 -0300

    fix: fixing navigation issue in Menu component

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -7,11 +7,11 @@ export default function Menu() {
 
   return (
       <View className="w-full flex flex-row items-center justify-around bg-blue-500 py-2">
-        <TouchableOpacity onPress={(navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3 bg-blue-600">
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3 bg-blue-600">
             <ClipboardList size={28} color='#fff' />
             <Text className="text-md mt-1 text-white">Tarefas</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={(navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3">
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className="flex items-center rounded-lg p-3">
             <Settings size={28} color='#fff' />
             <Text className="text-md mt-1 text-white">Configs</Text>
         </TouchableOpacity>
