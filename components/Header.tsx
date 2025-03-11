2013f5ad1ad3025c95fba5f74ef6ec0eef68645a
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-11 07:44:32 -0300

    refactoring and organizing elements in the header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,10 @@ export default function Header() {
   const navigation = useNavigation();
 
   return (
-      <View className="w-full flex flex-row items-center justify-between bg-blue-500 pt-6 pb-2 px-4">
-        <Text className="text-3xl text-white font-semibold">Lumina</Text>
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")}>
-            <Image source={require("../assets/images/userIcon.png")} className="h-16 w-16 rounded-full" />
+      <View className="w-full flex flex-row items-center justify-between bg-blue-500 h-24 px-4">
+        <Text className="text-3xl text-white font-semibold mt-3">Lumina</Text>
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className="mt-3">
+            <Image source={require("../assets/images/userIcon.png")} className="h-14 w-14 rounded-full" />
         </TouchableOpacity>
       </View>
   );
