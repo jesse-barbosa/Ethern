eddf7e72bb39e51570cf7824353d2608a7b911b5
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-12 22:25:58 -0300

    using 'env' instead Expo EAS to save variables

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,11 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = process.env.SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
-
-// Verifique se as variáveis estão definidas
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error('As variáveis de ambiente do Supabase não estão definidas.');
-}
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
