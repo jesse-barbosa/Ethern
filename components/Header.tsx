1d407b1fd76a67478d0abac04ade33f7e2dcc3ba
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-20 22:39:01 -0300

    changes to Header spacing and add app logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,13 @@ export default function Header() {
   const navigation = useNavigation();
 
   return (
-      <View className="w-full flex flex-row items-center justify-between bg-blue-500 h-24 px-4">
-        <Text className="text-3xl text-white font-semibold mt-3">Lumina</Text>
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className="mt-3">
-            <Image source={require("../assets/images/userIcon.png")} className="h-14 w-14 rounded-full" />
+      <View className="w-full flex flex-row items-center justify-between bg-blue-500 py-4 px-3">
+        <View className="flex flex-row items-center gap-3">
+          <Image source={require("../assets/icon.png")} className="h-10 w-10" />
+          <Text className="text-3xl text-white font-semibold ">Lumina</Text>
+        </View>
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className="">
+            <Image source={require("../assets/images/userIcon.png")} className="h-12 w-12 rounded-full" />
         </TouchableOpacity>
       </View>
   );
