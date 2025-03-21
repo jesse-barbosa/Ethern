3d398b24383fa6a2f72465fdadfda5abc79f04a4
jesse-barbosa <barbosajesse419@gmail.com>
2025-03-20 22:38:41 -0300

    changes to Menu spacing and height

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -7,14 +7,14 @@ export default function Menu() {
   const route = useRoute();
 
   return (
-      <View className="w-full flex flex-row items-center justify-around bg-blue-500 py-2">
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className={`${route.name === "Home" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-3`}>
+      <View className="w-full flex flex-row items-center justify-around bg-blue-500 py-2 mt-2">
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Home")} className={`${route.name === "Home" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-1`}>
             <MaterialIcons name="list-alt" size={28} color='#fff' />
-            <Text className="text-md mt-1 text-white">Tarefas</Text>
+            <Text className="text-md text-white">Tarefas</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className={`${route.name === "Settings" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-3`}>
+        <TouchableOpacity onPress={() => (navigation as any).navigate("Settings")} className={`${route.name === "Settings" ? 'bg-blue-600' : ''} flex items-center rounded-lg p-1`}>
             <MaterialIcons name="settings" size={28} color='#fff' />
-            <Text className="text-md mt-1 text-white">Configs</Text>
+            <Text className="text-md text-white">Configs</Text>
         </TouchableOpacity>
       </View>
   );
