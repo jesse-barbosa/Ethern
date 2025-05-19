64dd11e2c3cc1af442d3f84137a288b0eb3fcaec
jesse-barbosa <barbosajesse419@gmail.com>
2025-05-19 18:51:15 -0300

    maintenance: update expo SDK version (52 -> 53) and fix project dependency conflicts

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,13 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs"];
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  "react-native": require.resolve("react-native"),
+};
+config.transformer.unstable_allowRequireContext = true;
+
+module.exports = withNativeWind(config, { input: "./global.css" });
